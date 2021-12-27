96af2c62f0492c523547ccd3236edc7231002133
Max Braun <max.braun@gmail.com>
2021-12-26 22:25:16 -0500

    Add permalinks with browser history

--- frontend/main.py
+++ frontend/main.py
@@ -3,11 +3,22 @@ from flask import render_template
 from flask import send_from_directory
 from flask_minify import decorators
 from flask_minify import minify
-
 from google.cloud import bigquery
+import re
 
 app = Flask(__name__)
 minify(app=app, caching_limit=0, passive=True)
+
+
+def validate_id(id):
+    if not id:
+        # The ID must be non-empty.
+        return False
+
+    # The ID must match a regular expression.
+    pattern = re.compile(r'^[0-9a-f]{12}$')
+    match = pattern.match(id)
+    return bool(match)
 
 
 def random_proposition():
@@ -15,43 +26,73 @@ def random_proposition():
 
 
 def find_proposition(id):
-    client = bigquery.Client()
+    # Only allow well-formed IDs in the query.
+    if not validate_id(id):
+        id = None
 
+    # Compose the query.
+    client = bigquery.Client()
     query = ('SELECT id, german, english '
              'FROM tractatus.propositions ')
-
     if id:
+        # Pick the proposition with the provided ID.
         query += "WHERE id = '%s' " % id
     else:
+        # Pick a random proposition matching basic requirements.
         query += ("WHERE number = '8' "
                   "ORDER BY RAND() ")
-
     query += 'LIMIT 1'
+
+    # Make the request.
     job = client.query(query)
     rows = job.result()
 
     try:
+        # Read the first row.
         row = next(rows)
-        return row.id, row.german, row.english
     except StopIteration:
-        # TODO
-        return '0', '?', '?'
+        # The query contained no propositions.
+        error = 'Not found'
+        return '', error, error
+
+    return row.id, row.german, row.english
 
 
-@app.route('/')
-@decorators.minify(html=True, js=True, cssless=True)
-def index():
-    id, german, english = random_proposition()
-    return render_template('index.html', id=id, german=german, english=english)
-
-
-@app.route('/proposition')
-def proposition():
-    id, german, english = random_proposition()
+def render_json(id, german, english):
     return {
         'id': id,
         'german': german,
         'english': english}
+
+
+@app.route('/')
+@decorators.minify(html=True, js=True, cssless=True)
+def random_page():
+    # Serve the main page with a random proposition.
+    id, german, english = random_proposition()
+    return render_template('index.html', id=id, german=german, english=english)
+
+
+@app.route('/<id>')
+@decorators.minify(html=True, js=True, cssless=True)
+def proposition_page(id):
+    # Serve the main page with a specific proposition (via its ID).
+    id, german, english = find_proposition(id)
+    return render_template('index.html', id=id, german=german, english=english)
+
+
+@app.route('/proposition')
+def random_json():
+    # Serve a random proposition as raw data.
+    id, german, english = random_proposition()
+    return render_json(id=id, german=german, english=english)
+
+
+@app.route('/proposition/<id>')
+def proposition_json(id):
+    # Serve a specific proposition (via its ID) as raw data.
+    id, german, english = find_proposition(id)
+    return render_json(id=id, german=german, english=english)
 
 
 @app.route('/robots.txt')
